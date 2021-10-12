143ac07259227e328935066a93377144303fc825
“Roman <rummaan@me.com>
2021-10-11 23:01:37 -0400

    new face

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-container">
+        <p className="app-title">FORECASH</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/TodayGroup/TodayGroup.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./TodayGroup.css";
+
+const TodayGroup = () => {
+  return (
+    <div className="today-group">
+      <p className="today-currency">$</p>
+      <p className="today-amount">250</p>
+    </div>
+  );
+};
+
+export default TodayGroup;
